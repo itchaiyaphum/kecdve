fdec632d279262464f4dd4eb29d42c9b2dbd47d0
wannapong kumjumpon <wannapong.kjp@gmail.com>
2016-03-03 17:00:06 +0700

    add config production

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://dev.internship.phalconhost.com/';
+$config['base_url'] = 'http://internship.phalconhost.com/';
 
 /*
 |--------------------------------------------------------------------------

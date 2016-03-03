d4649bf657a526b80374aa276ca9ac1fc12bba3f
wannapong kumjumpon <wannapong.kjp@gmail.com>
2016-03-03 18:57:07 +0700

    fix check environment

--- index.php
+++ index.php
@@ -60,9 +60,6 @@ switch ($_SERVER ['SERVER_NAME']) {
         error_reporting ( E_ALL );
         ini_set ( 'display_errors', 'on' );
         break;
-    case 'internship.phalconhost.com' :
-        $env = 'testing';
-        break;
     default :
         $env = 'production';
 }

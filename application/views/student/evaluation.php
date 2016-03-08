cc49a5353dd32e9107f6dc34866658255ea42e1a
wannapong kumjumpon <wannapong.kjp@gmail.com>
2016-03-08 14:56:18 +0700

    update evaluation

--- application/views/student/evaluation.php
+++ application/views/student/evaluation.php
@@ -7,42 +7,30 @@
 			<div class="uk-text-center">
 				<h2>สรุปผลการเรียนประเมินการฝึกงาน/เรียนในสถานประกอบการ</h2>
 				<hr>
-				<h4>(Wannapong Kumjumpon ประจำปีการศึกษา 1/2558)</h4>
 			</div>
 			<br>
 			<div>
 				<h2>1. สำหรับสถานประกอบการ (80%)</h2>
-				<table class="uk-table uk-table-hover uk-table-condensed">
+				<table class="uk-table uk-table-condensed">
 					<tbody><tr>
 						<td>รายวิชา</td>
 						<td>คะแนนเต็ม</td>
 						<td>คะแนนที่ได้</td>
 						<td>คะแนนสุทธิ (80%)</td>
 					</tr>
-					<tr>
-						<td>3000-7001</td>
-						<td>80</td>
-						<td>75</td>
-						<td>75</td>
-					</tr>
+					
 				</tbody></table>
 			</div>
 			
 			<br>
 			<div>
 				<h2>2. สำหรับอาจารย์ประจำวิชา (20%)</h2>
-				<table class="uk-table uk-table-hover uk-table-condensed">
+				<table class="uk-table uk-table-condensed">
 					<tbody><tr>
 						<td>รายวิชา</td>
 						<td>คะแนนเต็ม</td>
 						<td>คะแนนที่ได้</td>
 						<td>คะแนนสุทธิ (20%)</td>
-					</tr>
-					<tr>
-						<td>3000-7001</td>
-						<td>20</td>
-						<td>17</td>
-						<td>17</td>
 					</tr>
 				</tbody></table>
 			</div>
@@ -50,20 +38,13 @@
 			<br>
 			<div>
 				<h2>3. ผลการประเมิน</h2>
-				<table class="uk-table uk-table-hover uk-table-condensed">
+				<table class="uk-table uk-table-condensed">
 					<tbody><tr>
 						<td>รายวิชา</td>
 						<td>คะแนนสุทธิ (80%)</td>
 						<td>คะแนนสุทธิ (20%)</td>
 						<td>คะแนนรวม (100%)</td>
 						<td>ผลการเรียนที่ได้</td>
-					</tr>
-					<tr>
-						<td>3000-7001</td>
-						<td>75</td>
-						<td>17</td>
-						<td>92</td>
-						<td>4</td>
 					</tr>
 				</tbody></table>
 			</div>

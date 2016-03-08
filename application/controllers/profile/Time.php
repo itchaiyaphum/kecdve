1715e7a9791d519eb0377d5e7b6b02b40c6a9c68
wannapong kumjumpon <wannapong.kjp@gmail.com>
2016-03-08 14:47:28 +0700

    add time feature

--- application/controllers/profile/Time.php
+++ application/controllers/profile/Time.php
@@ -1,14 +1,65 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Time extends CI_Controller {
-    public function __construct(){
+class Time extends CI_Controller
+{
+
+    public function __construct()
+    {
         parent::__construct();
+        
+        $this->load->helper(array(
+            'form',
+            'url'
+        ));
+        $this->load->library('form_validation');
+        $this->load->library('tank_auth');
+        $this->lang->load('tank_auth');
     }
-	public function index()
-	{
-	    $this->load->view('nav');
-		$this->load->view('student/time');
-		$this->load->view('footer');
-	}
+
+    public function index()
+    {
+        if (! $this->tank_auth->is_logged_in()) { // not logged in or not activated
+            redirect('/auth/login/');
+        } else {
+            $profile = $this->profile_lib->getData();
+            $day = $this->input->get_post('day', 1);
+            $week = $this->input->get_post('week', 1);
+            
+            $this->form_validation->set_rules('come_hour', 'เวลามา (ชั่วโมง)', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('come_minute', 'เวลามา (นาที)', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('back_hour', 'เวลากลับ (ชั่วโมง)', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('back_minute', 'เวลากลับ (นาที)', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('remark', 'หมายเหตุ', 'trim|xss_clean');
+            $this->form_validation->set_rules('remark_text', 'อื่นๆ', 'trim|xss_clean');
+            
+            $data['errors'] = array();
+            if ($this->form_validation->run()) { // validation ok
+                if ($this->time_lib->save(array(
+                    'user_id' => $profile->user_id,
+                    'day' => $day,
+                    'week' => $week,
+                    'come_hour' => $this->form_validation->set_value('come_hour'),
+                    'come_minute' => $this->form_validation->set_value('come_minute'),
+                    'back_hour' => $this->form_validation->set_value('back_hour'),
+                    'back_minute' => $this->form_validation->set_value('back_minute'),
+                    'remark' => $this->form_validation->set_value('remark'),
+                    'remark_text' => $this->form_validation->set_value('remark_text')
+                ))) { // success
+                    redirect('profile/time/');
+                } else { // fail
+                    $errors = $this->tank_auth->get_error_message();
+                    foreach ($errors as $k => $v)
+                        $data['errors'][$k] = $this->lang->line($v);
+                }
+            }
+            
+            $data = array();
+            $data['items'] = $this->time_lib->getItems($profile->user_id);
+            
+            $this->load->view('nav');
+            $this->load->view('student/time', $data);
+            $this->load->view('footer');
+        }
+    }
 }
--- application/views/student/time.php
+++ application/views/student/time.php
@@ -15,9 +15,8 @@
             		$days_of_weeks = array('จันทร์','อังคาร','พุธ','พฤหัสบดี','ศุกร์','เสาร์','อาทิตย์');
             		for($i=0; $i<18; $i++){
             		?>
-            		<article class="uk-article">
             			<table class="uk-table">
-            				<tbody><tr>
+            				<tr>
             					<td class="uk-width-1-10">สัปดาห์ที่ <?php echo ($i+1);?></td>
             					<td class="uk-width-1-10">วัน</td>
             					<td class="uk-width-2-10">เวลามา</td>
@@ -25,66 +24,88 @@
             					<td class="uk-width-3-10">หมายเหตุ</td>
             					<td class="uk-width-1-10" colspan="2"></td>
             				</tr>
-            				<?php 
-                    		for($j=0; $j<count($days_of_weeks); $j++){
-                    		?>
+            			</table>
+            			<?php 
+                    	for($j=0; $j<count($days_of_weeks); $j++){
+                    		    
+                		    $come_hour = 0;
+                		    $come_minute = 0;
+                		    $back_hour = 0;
+                		    $back_minute = 0;
+                		    $remark = 0;
+                		    $remark_text = "";
+                		    for($k=0; $k<count($items); $k++){
+                		        $item = $items[$k];
+                		        if($item->week==($i+1) && $item->day==($j+1)){
+                		            $come_hour = $item->come_hour;
+                        		    $come_minute = $item->come_minute;
+                        		    $back_hour = $item->back_hour;
+                        		    $back_minute = $item->back_minute;
+                        		    $remark = $item->remark;
+                        		    $remark_text = $item->remark_text;
+                		        }
+                		    }
+                    	?>
+                    	<form action="<?php base_url('profile/time'); ?>" class="uk-form uk-form-horizontal" method="post">
+                    	<table class="uk-table">
             				<tr>
-            					<td></td>
-            					<td><?php echo $days_of_weeks[$j];?></td>
-            					<td>
+            					<td class="uk-width-1-10"></td>
+            					<td class="uk-width-1-10"><?php echo $days_of_weeks[$j];?></td>
+            					<td class="uk-width-2-10">
             						<select name="come_hour" style="width: 50px; float: left;">
                                 		<?php 
                                 		for($ii=0; $ii<24; $ii++){
                                 		?>
-                                		<option value="<?php echo $ii;?>"><?php echo $ii;?></option>
+                                		<option value="<?php echo $ii;?>" <?php echo ($ii==$come_hour)?' selected="selected" ':''; ?>><?php echo $ii;?></option>
                                 		<?php } ?>
                                 	</select>
                                 	<select name="come_minute" style="width: 50px; float: left; margin-left: 5px;">
                                 		<?php 
                                 		for($ii=0; $ii<60; $ii++){
                                 		?>
-                                		<option value="<?php echo $ii;?>"><?php echo $ii;?></option>
+                                		<option value="<?php echo $ii;?>" <?php echo ($ii==$come_minute)?' selected="selected" ':''; ?>><?php echo $ii;?></option>
                                 		<?php } ?>
                                 	</select>
                                 </td>
-            					<td>
-            						<select name="come_hour" style="width: 50px; float: left;">
+            					<td class="uk-width-2-10">
+            						<select name="back_hour" style="width: 50px; float: left;">
                                 		<?php 
                                 		for($ii=0; $ii<24; $ii++){
                                 		?>
-                                		<option value="<?php echo $ii;?>"><?php echo $ii;?></option>
+                                		<option value="<?php echo $ii;?>" <?php echo ($ii==$back_hour)?' selected="selected" ':''; ?>><?php echo $ii;?></option>
                                 		<?php } ?>
                                 	</select>
-            						<select name="come_minute" style="width: 50px; float: left; margin-left: 5px;">
+            						<select name="back_minute" style="width: 50px; float: left; margin-left: 5px;">
                                 		<?php 
                                 		for($ii=0; $ii<60; $ii++){
                                 		?>
-                                		<option value="<?php echo $ii;?>"><?php echo $ii;?></option>
+                                		<option value="<?php echo $ii;?>" <?php echo ($ii==$back_minute)?' selected="selected" ':''; ?>><?php echo $ii;?></option>
                                 		<?php } ?>
                                 	</select>
             					</td>
-            					<td>
-            						<form class="uk-form">
-                						<div class="uk-form-row">
-                    						<select style="width: 100%;">
-                    							<option value="0"></option>
-                    							<option value="1">วันหยุดราชการ</option>
-                    							<option value="2">วันหยุดสถานประกอบการ</option>
-                    							<option value="3">ทำงานนอกเวลา</option>
-                    							<option value="4">อื่นๆ</option>
-                    						</select>
-                    						<textarea style="height: 30px; width: 100%;"></textarea>
-                						</div>
-            						</form>
+            					<td class="uk-width-3-10">
+            						<div class="uk-form-row">
+                						<select style="width: 100%;" name="remark">
+                							<option value="0" <?php echo ($remark==0)?' selected="selected" ':''; ?>></option>
+                							<option value="1" <?php echo ($remark==1)?' selected="selected" ':''; ?>>วันหยุดราชการ</option>
+                							<option value="2" <?php echo ($remark==2)?' selected="selected" ':''; ?>>วันหยุดสถานประกอบการ</option>
+                							<option value="3" <?php echo ($remark==3)?' selected="selected" ':''; ?>>ทำงานนอกเวลา</option>
+                							<option value="4" <?php echo ($remark==4)?' selected="selected" ':''; ?>>อื่นๆ</option>
+                						</select>
+                						<textarea style="height: 30px; width: 100%;" name="remark_text"><?php echo $remark_text;?></textarea>
+            						</div>
             					</td>
-            					<td><a href="<?php echo base_url('profile/time'); ?>" class="uk-button uk-button-small uk-button-success"><i class="uk-icon-pencil"></i> บันทึก</a></td>
+            					<td class="uk-width-1-10">
+            						<input type="submit" value="บันทึก" class="uk-button uk-button-small uk-button-success">
+            					</td>
             				</tr>
-            				<?php } ?>
-            				</tbody>
             			</table>
-            		</article>
+                		<input type="hidden" value="<?php echo ($i+1);?>" name="week"/>
+                		<input type="hidden" value="<?php echo ($j+1);?>" name="day"/>
+                		</form>
+            			<?php } ?>
             		<?php } ?>
-            		</div>
-                </div>
+        	</div>
+    	</div>
 	</div>
 </div>

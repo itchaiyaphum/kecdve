524d1c4751915d0b42fdeae59c12e06b0a246fc0
wannapong kumjumpon <wannapong.kjp@gmail.com>
2016-03-08 23:17:49 +0700

    update icon settings

--- application/views/settings/menu.php
+++ application/views/settings/menu.php
@@ -10,10 +10,10 @@ $profileData = $this->profile_lib->getData();
 <ul class="tm-nav uk-nav" data-uk-nav="">
 	<li class="uk-nav-header">เมนูหลัก</li>
 	<li class="<?php echo $this->helper_lib->getActiveMenu('profile');?>">
-		<a href="<?php echo base_url('settings/profile'); ?>"><span class="uk-icon-home"></span> แก้ไขข้อมูล</a>
+		<a href="<?php echo base_url('settings/profile'); ?>"><span class="uk-icon-pencil"></span> แก้ไขข้อมูลส่วนตัว</a>
 	</li>
 	<li class="<?php echo $this->helper_lib->getActiveMenu('password');?>">
-		<a href="<?php echo base_url('settings/password/'); ?>"><span class="uk-icon-book"></span> แก้ไขรหัสผ่าน</a>
+		<a href="<?php echo base_url('settings/password/'); ?>"><span class="uk-icon-lock"></span> แก้ไขรหัสผ่าน</a>
 	</li>
 </ul>
 <br/><br/>

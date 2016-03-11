0afb9a41149a038b8b95cd8c2ca648ee5862c31b
wannapong kumjumpon <wannapong.kjp@gmail.com>
2016-03-12 01:46:48 +0700

    Add admin/internship completed

---
+++ application/views/admin/internship/form.php
@@ -0,0 +1,108 @@
+<div class="uk-container uk-container-center">
+	<div class="uk-grid">
+		<div class="tm-sidebar uk-width-medium-1-4 uk-hidden-small">
+			<?php echo $leftmenu;?>
+		</div>
+		<div class="tm-main uk-width-medium-3-4 uk-margin-top uk-margin-bottom">
+            <form action="<?php echo base_url('admin/internship/edit');?>" method="post" name="adminForm" class="uk-form uk-form-horizontal">
+            
+    			<div class="uk-clearfix">
+    				<div class="uk-float-left">
+    					<h1>จัดการข้อมูลการฝึกงาน [<?php echo (is_null($item->id))?'เพิ่ม':'แก้ไข';?>]</h1>
+    				</div>
+        			<div class="uk-float-right">
+        				<input type="submit" value="บันทึกข้อมูล" class="uk-button uk-button-success"/>
+        				<a href="<?php echo base_url('/admin/internship/');?>" class="uk-button uk-button-danger">ยกเลิก</a>
+        			</div>
+    			</div>
+    			<hr/>
+    			
+            	<div class="uk-form-row">
+                    <label class="uk-form-label" for="form-h-it">ชื่อหัวข้อการฝึกงาน</label>
+                    <div class="uk-form-controls">
+                        <input type="text" id="form-h-it" name="title" value="<?php echo $item->title;?>" class="uk-width-1-2">
+                    	<div>
+                        	<?php echo form_error('title'); ?>
+                        	<?php echo isset($errors['title'])?$errors['title']:''; ?>
+                        </div>
+                    </div>
+                </div>
+            	<div class="uk-form-row">
+                    <label class="uk-form-label" for="form-h-it">วันที่เริ่มต้นการฝึกงาน</label>
+                    <div class="uk-form-controls">
+                        <input type="text" id="form-h-it" name="internship_start" value="<?php echo $item->internship_start;?>" class="uk-width-1-4">
+                    	<div>
+                        	<?php echo form_error('internship_start'); ?>
+                        	<?php echo isset($errors['internship_start'])?$errors['internship_start']:''; ?>
+                        </div>
+                    </div>
+                </div>
+            	<div class="uk-form-row">
+                    <label class="uk-form-label" for="form-h-it">วันที่สิ้นสุดการฝึกงาน</label>
+                    <div class="uk-form-controls">
+                        <input type="text" id="form-h-it" name="internship_end" value="<?php echo $item->internship_end;?>" class="uk-width-1-4">
+                    	<div>
+                        	<?php echo form_error('internship_end'); ?>
+                        	<?php echo isset($errors['internship_end'])?$errors['internship_end']:''; ?>
+                        </div>
+                    </div>
+                </div>
+            	<div class="uk-form-row">
+                    <label class="uk-form-label" for="form-h-it">จำนวนสัปดาห์การฝึกงาน</label>
+                    <div class="uk-form-controls">
+                        <input type="text" id="form-h-it" name="num_weeks" value="<?php echo $item->num_weeks;?>" class="uk-width-1-4"> สัปดาห์
+                    	<div>
+                        	<?php echo form_error('num_weeks'); ?>
+                        	<?php echo isset($errors['num_weeks'])?$errors['num_weeks']:''; ?>
+                        </div>
+                    </div>
+                </div>
+            	<div class="uk-form-row">
+                    <label class="uk-form-label" for="form-h-it">ระดับการศึกษา</label>
+                    <div class="uk-form-controls">
+                        <select name="edulevel_id"  class="uk-width-1-2">
+                        	<option value="0">- เลือกระดับการศึกษา -</option>
+                        	<?php 
+                        	for($i=0; $i<count($edulevel_items); $i++){
+                        	    $row = $edulevel_items[$i];
+                        	?>
+                        	<option value="<?php echo $row->id;?>" <?php echo ($row->id==$item->edulevel_id)?'selected="selected"':'';?>><?php echo $row->name;?></option>
+                        	<?php } ?>
+                        </select>
+                    </div>
+                </div>
+            	<div class="uk-form-row">
+                    <label class="uk-form-label" for="form-h-it">ภาคการศึกษา</label>
+                    <div class="uk-form-controls">
+                        <select name="semester_id"  class="uk-width-1-2">
+                        	<option value="0">- เลือกภาคการศึกษา -</option>
+                        	<?php 
+                        	for($i=0; $i<count($semester_items); $i++){
+                        	    $row = $semester_items[$i];
+                        	?>
+                        	<option value="<?php echo $row->id;?>" <?php echo ($row->id==$item->semester_id)?'selected="selected"':'';?>><?php echo $row->name;?></option>
+                        	<?php } ?>
+                        </select>
+                    </div>
+                </div>
+            	<div class="uk-form-row">
+                    <label class="uk-form-label" for="form-h-it">สถานศึกษา</label>
+                    <div class="uk-form-controls">
+                        <select name="college_id"  class="uk-width-1-2">
+                        	<option value="0">- เลือกสถานศึกษา -</option>
+                        	<?php 
+                        	for($i=0; $i<count($college_items); $i++){
+                        	    $row = $college_items[$i];
+                        	?>
+                        	<option value="<?php echo $row->id;?>" <?php echo ($row->id==$item->college_id)?'selected="selected"':'';?>><?php echo $row->name;?></option>
+                        	<?php } ?>
+                        </select>
+                    </div>
+                </div>
+                
+            	<input type="hidden" name="id" value="<?php echo $item->id;?>" />
+            </form>
+
+		</div>
+	</div>
+</div>
---
+++ application/views/admin/internship/index.php
@@ -0,0 +1,124 @@
+<div class="uk-container uk-container-center">
+	<div class="uk-grid">
+		<div class="tm-sidebar uk-width-medium-1-4 uk-hidden-small">
+			<?php echo $leftmenu;?>
+		</div>
+		<div class="tm-main uk-width-medium-3-4 uk-margin-top uk-margin-bottom">
+			<div class="uk-clearfix">
+				<div class="uk-float-left">
+					<h1>จัดการข้อมูลการฝึกงาน</h1>
+				</div>
+    			<div class="uk-float-right">
+    				<a href="<?php echo base_url('/admin/internship/edit');?>" class="uk-button uk-button-success"><i class="uk-icon-plus"></i> เพิ่ม</a>
+    			</div>
+			</div>
+			<hr/>
+            <form action="<?php echo base_url('admin/internship');?>" method="post" name="adminForm">
+            	<table class="uk-table">
+            		<tr>
+            			<td width="100%">
+            				Filter:
+            				<input type="text" name="internship_filter_search" id="search" value="<?php echo set_value('internship_filter_search');?>" class="text_area" onchange="document.adminForm.submit();" />
+            				<button onclick="this.form.submit();">Go</button>
+            				<button onclick="document.getElementById('search').value='';this.form.submit();">Reset</button>
+            			</td>
+            			<td nowrap="nowrap">
+            				<select name="internship_filter_semester_id" onchange="document.adminForm.submit();">
+                            	<option value="">- เลือกภาคการศึกษา -</option>
+                            	<?php 
+                            	for($i=0; $i<count($semester_items); $i++){
+                            	    $row = $semester_items[$i];
+                            	?>
+                            	<option value="<?php echo $row->id;?>" <?php echo set_select('internship_filter_semester_id',$row->id);?>><?php echo $row->name;?></option>
+                            	<?php } ?>
+                            </select>
+            				<?php echo $this->helper_lib->getStatusHtml('internship_filter_status');?>
+            			</td>
+            		</tr>
+            	</table>
+            
+            	<table class="uk-table" cellpadding="1">
+            		<thead>
+            			<tr>
+            				<th width="5%" class="title">#</th>
+            				<th class="title">
+            					รายวิชา
+            				</th>
+            				<th class="title" width="20%">
+            					สถานะ
+            				</th>
+            				<th width="10%" class="title" nowrap="nowrap">
+            					รหัส (ID)
+            				</th>
+            				<th width="15%" class="title" nowrap="nowrap">
+            					-
+            				</th>
+            			</tr>
+            		</thead>
+            		<tfoot>
+            			<tr>
+            				<td colspan="10" class="uk-text-center">
+            					<ul class="uk-pagination"><?php echo $pagination->create_links(); ?></ul>
+            				</td>
+            			</tr>
+            		</tfoot>
+            		<tbody>
+            		<?php 
+            		if(count( $items )<=0){
+            		    echo '<tr><td colspan="6" class="uk-text-center"><p>ไม่มีข้อมูล</p></td></tr>';
+            		}else{
+            			$k = 0;
+            			for ($i=0, $n=count( $items ); $i < $n; $i++)
+            			{
+            				$row 	=& $items[$i];
+            				
+            				$per_page = $this->input->get_post('per_page',1);
+            				$link_edit = base_url('admin/internship/edit/?id='.$row->id.'&per_page='.$per_page);
+            				$link_restore = base_url('admin/internship/restore/?id='.$row->id.'&per_page='.$per_page);
+            				$link_trash = base_url('admin/internship/trash/?id='.$row->id.'&per_page='.$per_page);
+            				$link_delete = base_url('admin/internship/delete/?id='.$row->id.'&per_page='.$per_page);
+            				
+            				$status_link = base_url('admin/internship/unpublish/?id='.$row->id.'&per_page='.$per_page);
+            				if($row->published==0){
+            				    $status_link = base_url('admin/internship/publish/?id='.$row->id.'&per_page='.$per_page);
+            				}
+            			?>
+            			<tr class="<?php echo "row$k"; ?>">
+            				<td>
+            					<?php echo $this->helper_lib->getPaginationIndex($i+1);?>
+            				</td>
+            				<td>
+            					<a href="<?php echo $link_edit;?>"><?php echo $row->title; ?></a>
+            				</td>
+            				<td>
+            					<a href="<?php echo $status_link;?>"><?php echo $this->helper_lib->getStatusIcon($row->published);?></a>
+            				</td>
+            				<td>
+            					<?php echo $row->id; ?>
+            				</td>
+            				<td>
+            					<a href="<?php echo $link_edit;?>" class="uk-button uk-button-success uk-button-mini"><i class="uk-icon-pencil"></i></a>
+            					<?php 
+            					if($this->helper_lib->getFilterStatus('internship_filter_status')=='trash'){
+            					?>
+            					<a href="<?php echo $link_restore;?>" class="uk-button uk-button-primary uk-button-mini"><i class="uk-icon-reply"></i></a>
+            					<a href="<?php echo $link_delete;?>" class="uk-button uk-button-danger uk-button-mini"><i class="uk-icon-remove"></i></a>
+            					<?php }else{?>
+            					<a href="<?php echo $link_trash;?>" class="uk-button uk-button-primary uk-button-mini"><i class="uk-icon-trash"></i></a>
+            					<?php }?>
+            				</td>
+            			</tr>
+            		<?php
+            			$k = 1 - $k;
+            			}
+            		}
+            		?>
+            		</tbody>
+            	</table>
+            
+            	<input type="hidden" name="boxchecked" value="0" />
+            </form>
+
+		</div>
+	</div>
+</div>

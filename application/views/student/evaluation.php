4e74f4c79a6d1fc9eb94064de8adb497b47bf087
wannapong kumjumpon <wannapong.kjp@gmail.com>
2016-06-03 22:42:02 +0700

    mockup evaluation's user

--- application/views/student/evaluation.php
+++ application/views/student/evaluation.php
@@ -6,48 +6,142 @@
 		<div class="tm-main uk-width-medium-3-4 uk-margin-top uk-margin-bottom">
 			<div class="uk-text-center">
 				<h2>สรุปผลการเรียนประเมินการฝึกงาน/เรียนในสถานประกอบการ</h2>
-				<hr>
-			</div>
-			<br>
-			<div>
-				<h2>1. สำหรับสถานประกอบการ (80%)</h2>
-				<table class="uk-table uk-table-condensed">
-					<tbody><tr>
-						<td>รายวิชา</td>
-						<td>คะแนนเต็ม</td>
-						<td>คะแนนที่ได้</td>
-						<td>คะแนนสุทธิ (80%)</td>
-					</tr>
-					
-				</tbody></table>
+				<h3>(สำหรับนักศึกษาฝึกงานในสถานประกอบการ)</h3>
 			</div>
 			
-			<br>
-			<div>
-				<h2>2. สำหรับอาจารย์ประจำวิชา (20%)</h2>
-				<table class="uk-table uk-table-condensed">
-					<tbody><tr>
-						<td>รายวิชา</td>
-						<td>คะแนนเต็ม</td>
-						<td>คะแนนที่ได้</td>
-						<td>คะแนนสุทธิ (20%)</td>
-					</tr>
-				</tbody></table>
-			</div>
+			<hr>
+			<br/>
+			<table width="100%">
+				<tr>
+					<td><b>ชื่อสถานประกอบการ:</b></td>
+					<td>บริษัท ทินิคอร์น จำกัด</td>
+					<td><b>แผนก/งาน:</b></td>
+					<td>ซอฟต์แวร์</td>
+				</tr>
+				<tr>
+					<td><b>ชื่อสถานศึกษา:</b></td>
+					<td>วิทยาลัยเทคนิคชัยภูมิ</td>
+					<td><b>สาขา:</b></td>
+					<td>เทคโนโลยีสารสนเทศ</td>
+				</tr>
+				<tr>
+					<td><b>วันเริ่มต้นการฝึกงาน:</b></td>
+					<td>02/05/2559 - 02/08/2559</td>
+					<td></td>
+					<td></td>
+				</tr>
+			</table>
+			<br/>
+			<hr>
 			
-			<br>
-			<div>
-				<h2>3. ผลการประเมิน</h2>
-				<table class="uk-table uk-table-condensed">
-					<tbody><tr>
-						<td>รายวิชา</td>
-						<td>คะแนนสุทธิ (80%)</td>
-						<td>คะแนนสุทธิ (20%)</td>
-						<td>คะแนนรวม (100%)</td>
-						<td>ผลการเรียนที่ได้</td>
+			<table class="uk-table uk-table-border">
+				<thead>
+					<tr>
+						<th>สัปดาห์ที่</th>
+						<th>สรุปการมาปฏิบัติงาน</th>
+						<th>มา</th>
+						<th>สาย</th>
+						<th>ไม่มา</th>
+						<th>ลา</th>
+						<th>รวม</th>
+						<th>ครูนิเทศตรวจเยี่ยม</th>
+						<th>การบันทึกปฏิบัติงาน</th>
+						<th>ผู้ควบคุมตรวจยืนยัน</th>
 					</tr>
-				</tbody></table>
-			</div>
+				</thead>
+				<tr>
+					<td class="uk-text-center">1</td>
+					<td>02/05/2559 - 06/05/2559</td>
+					<td class="uk-text-center">5</td>
+					<td class="uk-text-center">-</td>
+					<td class="uk-text-center">-</td>
+					<td class="uk-text-center">-</td>
+					<td class="uk-text-center">5</td>
+					<td class="uk-text-center"><a href="#" class="uk-button uk-button-success">ตรวจแล้ว</a></td>
+					<td class="uk-text-center"><a href="#" class="uk-button uk-button-danger">ยังไม่ส่งบันทึก</a></td>
+					<td class="uk-text-center"><a href="#" class="uk-button uk-button-danger">ยังไม่ตรวจ</a></td>
+				</tr>
+				<tr>
+					<td class="uk-text-center">2</td>
+					<td>09/05/2559 - 13/05/2559</td>
+					<td class="uk-text-center">5</td>
+					<td class="uk-text-center">-</td>
+					<td class="uk-text-center">-</td>
+					<td class="uk-text-center">-</td>
+					<td class="uk-text-center">5</td>
+					<td class="uk-text-center"><a href="#" class="uk-button uk-button-success">ตรวจแล้ว</a></td>
+					<td class="uk-text-center"><a href="#" class="uk-button uk-button-danger">ยังไม่ส่งบันทึก</a></td>
+					<td class="uk-text-center"><a href="#" class="uk-button uk-button-danger">ยังไม่ตรวจ</a></td>
+				</tr>
+				<tr>
+					<td class="uk-text-center">3</td>
+					<td>16/05/2559 - 20/05/2559</td>
+					<td class="uk-text-center">5</td>
+					<td class="uk-text-center">-</td>
+					<td class="uk-text-center">-</td>
+					<td class="uk-text-center">-</td>
+					<td class="uk-text-center">5</td>
+					<td class="uk-text-center"><a href="#" class="uk-button uk-button-success">ตรวจแล้ว</a></td>
+					<td class="uk-text-center"><a href="#" class="uk-button uk-button-danger">ยังไม่ส่งบันทึก</a></td>
+					<td class="uk-text-center"><a href="#" class="uk-button uk-button-danger">ยังไม่ตรวจ</a></td>
+				</tr>
+				<tr>
+					<td colspan="2" class="uk-text-right">รวม:</td>
+					<td class="uk-text-center">15</td>
+					<td class="uk-text-center">0</td>
+					<td class="uk-text-center">0</td>
+					<td class="uk-text-center">0</td>
+					<td class="uk-text-center">15</td>
+					<td class="uk-text-center">3</td>
+					<td class="uk-text-center">0</td>
+					<td class="uk-text-center">0</td>
+				</tr>
+			</table>
+			
+			<br/>
+			<h2><u>สรุป</u></h2>
+			<div class="uk-grid">
+        		<div class="uk-width-medium-1-4">
+        			<h3>สรุปเวลาปฏิบัติงาน</h3>
+        			<ul class="uk-list uk-list-striped">
+        				<li>มา = 15 วัน</li>
+        				<li>ไม่มา = 3 วัน</li>
+        				<li>สาย = 5 วัน</li>
+        				<li>ลา = 1 วัน</li>
+        			</ul>
+        		</div>
+        		<div class="uk-width-medium-1-4">
+        			<h3>สรุปบันทึกปฏิบัติงาน</h3>
+        			<ul class="uk-list uk-list-striped">
+        				<li>ส่งบันทึกแล้ว = 15 วัน</li>
+        				<li>ยังไม่ส่งบันทึก = 3 วัน</li>
+        			</ul>
+        		</div>
+        		<div class="uk-width-medium-1-4">
+        			<h3>สรุปครูนิเทศน์ตรวจเยี่ยม</h3>
+        			<ul class="uk-list uk-list-striped">
+        				<li>ตรวจแล้ว = 15 วัน</li>
+        				<li>ยังไม่ตรวจ = 3 วัน</li>
+        			</ul>
+        		</div>
+        		<div class="uk-width-medium-1-4">
+        			<h3>สรุปผู้ควบคุมตรวจยืนยัน</h3>
+        			<ul class="uk-list uk-list-striped">
+        				<li>ตรวจแล้ว = 15 วัน</li>
+        				<li>ยังไม่ตรวจ = 3 วัน</li>
+        			</ul>
+        		</div>
+        	</div>
+        	
+        	<h2><u>หมายเหตุ</u></h2>
+        	<div>
+        		<span><u>การตรวจยืนยัน</u></span>
+        		บันทึกผล ทำโดยผู้ควบคุมการฝึกงาน หรือครูฝึกงานในสถานประกอบการ ให้การยืนยันในระบบออนไลน์
+        	</div>
+			<div>
+        		<span><u>การตรวจเยี่ยม</u></span>
+        		หมายถึง ครูนิเทศน์การฝึกงาน หรือ ครูที่ปรึกษา เข้าตรวจสอบในระบบออนไลน์
+        	</div>
 		</div>
 	</div>
 </div>
